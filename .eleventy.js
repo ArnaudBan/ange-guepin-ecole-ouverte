5cb08529bbf09e6a9aa043e59aba93cb27c8770e
Arnaud Banvillet <arnaud.banvillet@derniercri.io>
2022-01-30 11:59:04 +0100

    ajout de netlify CMS

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("./src/medias");
 
   const { DateTime } = require("luxon");
   eleventyConfig.addFilter("postDate", (dateObj) => {
